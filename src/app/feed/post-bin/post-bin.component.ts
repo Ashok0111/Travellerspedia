f096acef38de353631a2cbe8a727575329435a3b
Ashok0111 <ashok8182@gmail.com>
2019-06-23 18:20:42 +0530

    alignment fixed

--- src/app/feed/post-bin/post-bin.component.ts
+++ src/app/feed/post-bin/post-bin.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-bin.component.css']
 })
 export class PostBinComponent implements OnInit {
-
+  items=["test1","test2","test3"];
   constructor() { }
 
   ngOnInit() {
