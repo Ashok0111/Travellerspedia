c29d10c233201b4cdd96519ba2adf3d318a3d323
Ashok0111 <ashok8182@gmail.com>
2019-08-18 15:41:47 +0530

    styles added trimmed and added forget password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,14 +7,16 @@ import { ReginInteration } from '../interaction_service/regin_interation';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  _show_top:boolean=true;
   constructor(private regininteration: ReginInteration) {
     this.regininteration.navbar_landingpage(true);
     this.regininteration.feed_view(false);
    }
 
   ngOnInit() {
-  
   }
-
+  reset_email()
+  {
+    this._show_top=false;
+  }
 }
