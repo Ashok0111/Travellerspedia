79b100da1e832d6d332f19dd6af40ce8b852f2f1
Ashok0111 <ashok8182@gmail.com>
2019-09-03 08:15:26 +0530

    header authorization

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
+
 @Injectable()
 export class AuthService {
     public baseURL = environment.apiUrl;
-  constructor(public http : HttpClient) {}
+  constructor(public http : HttpClient) {
+
+  }
   // ...
+  
   public send_token()
   {
-    let token={
-        "token":localStorage.getItem('a-t')
+    let headers: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+    headers = headers.append('Authorization', localStorage.getItem('a-t'));
+    const httpOptions= {
+      headers: headers
     };
-    console.log(token,"Token");
-    return this.http.post(this.baseURL+"auth/guard/",token).toPromise();
+    return this.http.get(this.baseURL+"auth/validate/",httpOptions).toPromise();
   }
 }
