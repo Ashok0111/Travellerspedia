a8f4c884677f03b3bcd91327242364f63c6b0c16
ashok0111 <ashok8182@gmail.com>
2020-01-28 23:58:24 +0530

    login profile picture upload

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,7 +10,7 @@ import { ImageCroppedEvent } from 'ngx-image-cropper';
 export class ProfileComponent implements OnInit {
   imageChangedEvent: any = '';
   croppedImage: any = '';
- 
+ show_div:boolean=false;
   constructor(private regininteration: ReginInteration) { 
     this.regininteration.feed_view(true);
     this.regininteration.navbar_landingpage(true);
@@ -21,10 +21,20 @@ disp_name:string;
     this.disp_name=this.getDecodedAccessToken(u_d)['user_claims']['name'];
   }
   fileChangeEvent(event: any): void {
+this.show_div=true;
     this.imageChangedEvent = event;
 }
 imageCropped(event: ImageCroppedEvent) {
     this.croppedImage = event.base64;
+    
+}
+
+close_profilepic()
+{
+  console.log("profile close");
+  this.croppedImage='';
+  this.imageChangedEvent='';
+  this.show_div=false;
 }
 imageLoaded() {
     // show cropper
